ff89068ee1f3c68a61b8659ac7a430b3ea810a7d
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-04-01 08:49:17 +0900

    set step5

--- step5/lib/main.dart
+++ step5/lib/main.dart
@@ -1,4 +1,4 @@
-//XとOを交互に表示しよう
+//ゲーム勝者の判定をしよう
 import 'package:flutter/material.dart';
 
 void main() {
@@ -51,6 +51,9 @@ class _BoardState extends State<Board> {
 
   void handleClick(int i) {
     final squares = _squares.sublist(0);
+    if (calculateWinner(squares) != null || squares[i] != null) {
+      return;
+    }
     squares[i] = _xIsNext ? 'X' : 'O';
     setState(() {
       _squares = squares;
@@ -60,9 +63,16 @@ class _BoardState extends State<Board> {
 
   @override
   Widget build(BuildContext context) {
+    final winner = calculateWinner(_squares);
+    String status;
+    if (winner != null) {
+      status = 'Winner: $winner';
+    } else {
+      status = 'Next player: ${_xIsNext ? 'X' : 'O'}';
+    }
     return Column(
       children: [
-        Text('Next Player: ${_xIsNext ? 'X' : 'O'}'),
+        Text(status),
         SizedBox(
           height: 34 * 3,
           width: 34 * 3,
@@ -107,4 +117,26 @@ class Game extends StatelessWidget {
       ),
     );
   }
+}
+
+String? calculateWinner(List<String?> squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (var i = 0; i < lines.length; i++) {
+    final indexs = lines[i];
+    if (squares[indexs[0]] != null &&
+        squares[indexs[0]] == squares[indexs[1]] &&
+        squares[indexs[0]] == squares[indexs[2]]) {
+      return squares[indexs[0]];
+    }
+  }
+  return null;
 }
