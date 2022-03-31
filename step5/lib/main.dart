0d5f70430c5bb55a3becf9ed0191bc1b5de3f123
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-31 23:29:36 +0900

    add folder step5

---
+++ step5/lib/main.dart
@@ -0,0 +1,110 @@
+//XとOを交互に表示しよう
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Game());
+}
+
+class Square extends StatelessWidget {
+  const Square({
+    Key? key,
+    required this.onTap,
+    required this.value,
+  }) : super(key: key);
+
+  final void Function() onTap;
+  final String? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 34,
+        width: 34,
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color.fromRGBO(9, 9, 9, 1), width: 1),
+        ),
+        child: Center(
+          child: Text(
+            value ?? '',
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Board extends StatefulWidget {
+  const Board({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Board> createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  List<String?> _squares = List.generate(9, (index) => null);
+  bool _xIsNext = true;
+
+  void handleClick(int i) {
+    final squares = _squares.sublist(0);
+    squares[i] = _xIsNext ? 'X' : 'O';
+    setState(() {
+      _squares = squares;
+      _xIsNext = !_xIsNext;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Next Player: ${_xIsNext ? 'X' : 'O'}'),
+        SizedBox(
+          height: 34 * 3,
+          width: 34 * 3,
+          child: GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            children: List.generate(
+              9,
+              (int i) => Square(
+                onTap: () => handleClick(i),
+                value: _squares[i],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Game extends StatelessWidget {
+  const Game({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Board(),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(children: const []),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
