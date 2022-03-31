1f7ec28001db91fe8dc5993f9b09110fa91997c6
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-31 19:51:50 +0900

    add step 2

---
+++ step2/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Game());
+}
+
+class Square extends StatefulWidget {
+  const Square({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Square> createState() => _SquareState();
+}
+
+class _SquareState extends State<Square> {
+  String value = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => setState(() {
+        value = 'X';
+      }),
+      child: Container(
+        height: 34,
+        width: 34,
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color.fromRGBO(9, 9, 9, 1), width: 1),
+        ),
+        child: Center(
+          child: Text(
+            value,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Board extends StatelessWidget {
+  const Board({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Next Player: X'),
+        SizedBox(
+          height: 34 * 3,
+          width: 34 * 3,
+          child: GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            children: List.generate(
+              9,
+              (int i) => const Square(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Game extends StatefulWidget {
+  const Game({Key? key}) : super(key: key);
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Board(),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(children: const []),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
