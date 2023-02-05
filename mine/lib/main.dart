835b5c9f0b028e0b81f391e5de785a0edaf7d46f
katoy <youichikato@gmail.com>
2023-02-05 11:45:15 +0900

    Start mine

---
+++ mine/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Game());
+}
+
+class Square extends StatelessWidget {
+  const Square({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: 34,
+        width: 34,
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color.fromRGBO(9, 9, 9, 1), width: 1),
+        ),
+        child: const Center(
+          child: Text(
+            '',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Board extends StatelessWidget {
+  const Board({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const status = 'Next Player: X';
+    return Column(
+      children: [
+        const Text(status),
+        SizedBox(
+          height: 34 * 3,
+          width: 34 * 3,
+          child: GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            children: List.generate(
+              9,
+              (int i) => const Square(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Game extends StatelessWidget {
+  const Game({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Board(),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(children: const []),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
